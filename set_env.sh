c61684f05ca1864baf2bc6e5a222f87419072304
Sharan Khan <sharan.gohar@gmail.com>
2021-05-27 16:06:59 +0500

    aws credentials

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=
-export POSTGRES_PASSWORD=
-export POSTGRES_HOST=
-export POSTGRES_DB=
-export AWS_BUCKET=
-export AWS_REGION=
-export AWS_PROFILE=
+export POSTGRES_USERNAME=project2udacity
+export POSTGRES_PASSWORD=project2udacity
+export POSTGRES_HOST=project2udacity.c67ljcn1m8um.ap-south-1.rds.amazonaws.com
+export POSTGRES_DB=project2udacity
+export AWS_BUCKET=project2-332906374637-dev
+export AWS_REGION=ap-south-1
+export AWS_PROFILE=default
 export JWT_SECRET=hello
 export URL=http://localhost:8100
