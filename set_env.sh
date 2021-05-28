a075c36475fa3e2332cc915db7296fc8bafa33b9
Sharan Khan <sharan.gohar@gmail.com>
2021-05-28 11:15:16 +0500

    changed cds

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=project2udacity
-export POSTGRES_PASSWORD=project2udacity
-export POSTGRES_HOST=project2udacity.c67ljcn1m8um.ap-south-1.rds.amazonaws.com
-export POSTGRES_DB=project2udacity
+export POSTGRES_USERNAME=sharan
+export POSTGRES_PASSWORD=sharan123
+export POSTGRES_HOST=database1.c67ljcn1m8um.ap-south-1.rds.amazonaws.com
+export POSTGRES_DB=database1
 export AWS_BUCKET=project2-332906374637-dev
-export AWS_REGION=ap-south-1
+export AWS_REGION=ap-south-1b
 export AWS_PROFILE=default
 export JWT_SECRET=hello
 export URL=http://localhost:8100
